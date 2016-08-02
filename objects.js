578cffea6ab3fca8d5ee18180cf9f20b3a48d124
Farid <your_email@youremail.com>
2016-08-02 10:01:07 -0400

    Object examples

---
+++ objects.js
@@ -0,0 +1,19 @@
+//var foodPreferences = {
+      // pizza: 'yum',
+      // salad: 'gross'
+     //};
+    // console.log(foodPreferences);
+
+  var pizza = {
+    toppings: ['cheese','sauce','pepperoni'],
+    crust:  "deep dish",
+    serves: 2
+  };
+  console.log(pizza);
+  /*var pizza = {
+    toppings: ['cheese','sauce','pepperoni'],
+    crust:  "deep dish",
+    serves: 2
+  };
+  console.log(pizza.crust);
+*/
